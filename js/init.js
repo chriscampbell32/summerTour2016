b22a73cf469cee23796af5cc4215121c25d0025d
chriscampbell32 <chriscampbell32@yahoo.com>
2016-02-05 20:07:41 -0500

    added collapsibles for tour stops, will add more

--- js/init.js
+++ js/init.js
@@ -3,6 +3,8 @@
 
     $('.button-collapse').sideNav();
     $('.parallax').parallax();
+    $('.collapsible').collapsible()
+      
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
